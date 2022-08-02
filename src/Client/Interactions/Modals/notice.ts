be61a2674c727fc3eef011241a6c1ef6040dcc8c
Migan178 <migan9857@gmail.com>
2022-08-02 18:05:18 +0900

    feat: notice

---
+++ src/Client/Interactions/Modals/notice.ts
@@ -0,0 +1,40 @@
+import { ModalSubmitInteraction, EmbedBuilder, ChannelType } from 'discord.js'
+
+export default {
+  execute(interaction: ModalSubmitInteraction) {
+    interaction.client.channels.cache.forEach(channel => {
+      if (channel.type !== ChannelType.GuildText) return
+      if (
+        channel.topic?.includes(`${interaction.client.user?.username}-공지`)
+      ) {
+        channel.send({
+          embeds: [
+            new EmbedBuilder()
+              .setAuthor({
+                name: `공지 발송자: ${interaction.user.tag}`,
+                iconURL: interaction.user.displayAvatarURL(),
+              })
+              .setFields([
+                {
+                  name: interaction.fields.getTextInputValue(
+                    'Doremi-notice$title'
+                  ),
+                  value: interaction.fields.getTextInputValue(
+                    'Doremi-notice$content'
+                  ),
+                },
+              ]),
+          ],
+        })
+      }
+    })
+    interaction.reply({
+      content: `공지\n제목: ${interaction.fields.getTextInputValue(
+        'Doremi-notice$title'
+      )}\n내용: ${interaction.fields.getTextInputValue(
+        'Doremi-notice$content'
+      )}\n을 발송했습니다.`,
+      ephemeral: true,
+    })
+  },
+}
---
+++ src/Commands/info/notice.ts
@@ -0,0 +1,41 @@
+import { Command } from '../../Client'
+import {
+  ChatInputCommandInteraction,
+  ActionRowBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} from 'discord.js'
+
+export = class extends Command {
+  name = '공지'
+  description = '[개발자 전용] Doremi의 공지'
+  execute(interaction: ChatInputCommandInteraction) {
+    if (interaction.user.id !== process.env.OWNER_ID)
+      return interaction.reply({
+        content: '❌ 해당 명령어는 개발자만 사용 가능해요. :(',
+        ephemeral: true,
+      })
+
+    const modal = new ModalBuilder()
+      .setCustomId('Doremi-modal$notice')
+      .setTitle('공지 내용')
+
+    const noticeTitle = new ActionRowBuilder().addComponents(
+      new TextInputBuilder()
+        .setCustomId('Doremi-notice$title')
+        .setLabel('공지의 제목')
+        .setStyle(TextInputStyle.Short)
+    )
+
+    const noticeContent = new ActionRowBuilder().addComponents(
+      new TextInputBuilder()
+        .setCustomId('Doremi-notice$content')
+        .setLabel('공지의 내용')
+        .setStyle(TextInputStyle.Paragraph)
+    )
+
+    // @ts-ignore
+    interaction.showModal(modal.addComponents([noticeTitle, noticeContent]))
+  }
+}
