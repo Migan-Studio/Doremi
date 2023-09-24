01d73c017852c69e5071806d4c849c351969374b
Migan178 <migan9857@gmail.com>
2023-09-24 16:37:02 +0900

    feat: timeout

--- src/Commands/mod/timeout.ts
+++ src/Commands/mod/timeout.ts
@@ -3,6 +3,7 @@ import {
   ApplicationCommandOptionType,
   GuildMember,
   type ChatInputCommandInteraction,
+  type APIEmbed,
 } from 'discord.js'
 import { Command } from 'mbpr-rodule'
 import { dayRegex, dicimal, hourRegex, minRegex, weekRegex } from '@utils'
@@ -55,20 +56,82 @@ export default class TimeoutCommands extends Command {
     const locale = localizations(interaction.locale)
 
     if (member instanceof GuildMember) {
+      const embed: APIEmbed = {
+        title: locale.timeout.name,
+        description: locale.timeout.embeds.success.replace(
+          '{member}',
+          member.user.username,
+        ),
+        timestamp: new Date().toISOString(),
+        color: 0x00ff00,
+      }
       if (minRegex.test(time)) {
         const a = time.match(dicimal)![0] as unknown as number
         if (a > 40320)
           return interaction.reply({
             content: locale.timeout.embeds.max_value
-              .replace('{time}', `${a}`)
+              .replace('{time}', `40320`)
               .replace('{mhdw}', getSmhdw(interaction.locale, 'minute')),
             ephemeral: true,
           })
 
         member.disableCommunicationUntil(Date.now() + a * 60 * 1000, resaon!)
+
+        interaction.reply({
+          embeds: [embed],
+          ephemeral: true,
+        })
       } else if (hourRegex.test(time)) {
+        const a = time.match(dicimal)![0] as unknown as number
+        if (a > 672)
+          return interaction.reply({
+            content: locale.timeout.embeds.max_value
+              .replace('{time}', `672`)
+              .replace('{mhdw}', getSmhdw(interaction.locale, 'hour')),
+            ephemeral: true,
+          })
+
+        member.disableCommunicationUntil(Date.now() + a * 3600 * 1000, resaon!)
+
+        interaction.reply({
+          embeds: [embed],
+          ephemeral: true,
+        })
       } else if (dayRegex.test(time)) {
+        const a = time.match(dicimal)![0] as unknown as number
+        if (a > 28)
+          return interaction.reply({
+            content: locale.timeout.embeds.max_value
+              .replace('{time}', `28`)
+              .replace('{mhdw}', getSmhdw(interaction.locale, 'day')),
+            ephemeral: true,
+          })
+
+        member.disableCommunicationUntil(Date.now() + a * 86400 * 1000, resaon!)
+
+        interaction.reply({
+          embeds: [embed],
+          ephemeral: true,
+        })
       } else if (weekRegex.test(time)) {
+        const a = time.match(dicimal)![0] as unknown as number
+        if (a > 4)
+          return interaction.reply({
+            content: locale.timeout.embeds.max_value
+              .replace('{time}', `4`)
+              .replace('{mhdw}', getSmhdw(interaction.locale, 'week')),
+            ephemeral: true,
+          })
+
+        member.disableCommunicationUntil(
+          Date.now() + a * 604_800 * 1000,
+          resaon!,
+        )
+
+        interaction.reply({
+          embeds: [embed],
+          ephemeral: true,
+        })
       } else
         return interaction.reply({
           embeds: [
--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -3,4 +3,4 @@ export const minRegex = /^[0-9]{1,2}(?:\s)?(?:분|min|m|minute|minutes)$/i
 export const hourRegex = /^[0-9]{1,2}(?:\s)?(?:시간|hour|h|hours)$/i
 export const dayRegex = /^[0-9]{1,2}(?:\s)?(?:일|day|d|days)$/i
 export const weekRegex = /^[0-9]{1,2}(?:\s)?(?:주|week|w|weeks)$/i
-export const dicimal = /^[0-9]$/
+export const dicimal = /^[0-9]/g
